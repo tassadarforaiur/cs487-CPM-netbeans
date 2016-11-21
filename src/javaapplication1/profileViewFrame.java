ed130ab3aa181e52ef2144ab815577696aa83322
aviverit <andrew.viverito@gmail.com>
2016-11-21 17:59:54 -0600

    MoreEventsFrame close property set to DISPOSE (401 response issue returned)

--- src/javaapplication1/profileViewFrame.java
+++ src/javaapplication1/profileViewFrame.java
@@ -316,14 +316,11 @@ public class profileViewFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {                                         
         close();
         mainMenuFrame mMF = new mainMenuFrame();
         mMF.setVisible(true);
-    }                                     
+    }//GEN-LAST:event_jButton2ActionPerformed
+                                   
     /**
      * @param args the command line arguments
      */
